d1ed30c428b0fe6f06ad21bdabd1e1ef212d44d9
Sean Doyle <sean.p.doyle24@gmail.com>
2021-04-07 09:17:57 -0400

    Searching Article content with SQL ILIKE
    Postgres supports the [LIKE and ILIKE][] keywords for loose pattern
    matching:
    
    ```
    string LIKE pattern [ESCAPE escape-character]
    ```
    
    > The `LIKE` expression returns `true` if the `*string*` matches the
    > supplied `*pattern*`.
    
    > If `pattern` does not contain percent signs or underscores, then the
    > `pattern` only represents the `string` itself; in that case `LIKE`
    > acts like the equals operator. An underscore (`_`) in pattern stands
    > for (matches) any single character; a percent sign (`%`) matches any
    > sequence of zero or more characters.
    
    To test it out, declare [has_rich_text :content][has_rich_text] on the
    `Article` model, along with a `with_content_containing` scope that joins
    onto the related [ActionText::RichText][] model, and implemented with an
    `ILIKE` query:
    
    ```
    --- a/app/models/article.rb
    +++ b/app/models/article.rb
     class Article < ApplicationRecord
       has_rich_text :content
    +
    +  scope :with_content_containing, ->(query) { joins(:rich_text_content).merge(ActionText::RichText.where <<~SQL, "%" + query + "%") }
    +    body ILIKE ?
    +  SQL
     end
    ```
    
    Using an `ILIKE` clause achieves our desired outcome, but has some
    quirks and drawbacks. For instance, `ILIKE` won't account for any HTML
    characters, so while a search for `"needle in the haystack"` matches
    content like `<div>The text of this article matches has the needle in
    the haystack<div>`, it won't match content with HTML interspersed (e.g.
    `<div>The text of this article matches has the <strong>needle</strong>
    in the haystack</div>`.
    
    [LIKE and ILIKE]: https://www.postgresql.org/docs/12/functions-matching.html#FUNCTIONS-LIKE
    [has_rich_text]: https://edgeapi.rubyonrails.org/classes/ActionText/Attribute.html#method-i-has_rich_text
    [ActionText::RichText]: https://edgeapi.rubyonrails.org/classes/ActionText/RichText.html

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   has_rich_text :content
+
+  scope :with_content_containing, ->(query) { joins(:rich_text_content).merge(ActionText::RichText.where <<~SQL, "%" + query + "%") }
+    body ILIKE ?
+  SQL
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "searches all Articles containing a query" do
+    includes_query = Article.create! title: "Includes the query", content: <<~HTML
+      <div>The text of this article matches has the needle in the haystack</div>
+    HTML
+    excludes_query = Article.create! title: "Excludes the query", content: <<~HTML
+      <div>The text of this article matches is unrelated!</div>
+    HTML
+
+    search_results = Article.with_content_containing "needle in the haystack"
+
+    assert_includes search_results.pluck(:title), includes_query.title
+    assert_not_includes search_results.pluck(:title), excludes_query.title
+  end
 end
